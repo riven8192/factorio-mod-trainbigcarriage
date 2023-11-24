bd676ae9a25fd89e31111ff45e46c9ecf6d2a0a3
Riven8192 <jgo@kataf.nl>
2023-11-24 13:03:13 +0100

    Release 0.1.1 - Hardcoding item prototype names

--- TrainBigCarriage/data.lua
+++ TrainBigCarriage/data.lua
@@ -2,20 +2,9 @@ require("prototypes.item")
 	
 local storageMultiplier = 4;
 
-for name, table in pairs(data.raw["cargo-wagon"]) do
-	if table.inventory_size then
-		table.inventory_size = math.ceil(table.inventory_size * storageMultiplier)
-	end
-	if table.weight then
-		table.weight = math.ceil(table.weight * storageMultiplier)
-	end
-end
+data.raw["cargo-wagon"]["cargo-wagon"].inventory_size = math.ceil(data.raw["cargo-wagon"]["cargo-wagon"].inventory_size * storageMultiplier)
+data.raw["cargo-wagon"]["cargo-wagon"].weight         = math.ceil(data.raw["cargo-wagon"]["cargo-wagon"].weight         * storageMultiplier)
 
-for name, table in pairs(data.raw["fluid-wagon"]) do
-	if table.fluid_capacity then
-		table.fluid_capacity = math.ceil(table.fluid_capacity * storageMultiplier)
-	end
-	if table.weight then
-		table.weight = math.ceil(table.weight * storageMultiplier)
-	end
-end
+data.raw["fluid-wagon"]["fluid-wagon"].capacity       = math.ceil(data.raw["fluid-wagon"]["fluid-wagon"].capacity       * storageMultiplier)
+data.raw["fluid-wagon"]["fluid-wagon"].weight         = math.ceil(data.raw["fluid-wagon"]["fluid-wagon"].weight         * storageMultiplier)
+
