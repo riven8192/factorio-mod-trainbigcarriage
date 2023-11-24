00e8f2bfdc2163ae0ba2cc52cda95a346447bd56
Riven8192 <jgo@kataf.nl>
2023-11-24 13:04:13 +0100

    Release 0.1.2 - Remove weight adjustment of carriages/wagons

--- TrainBigCarriage/data.lua
+++ TrainBigCarriage/data.lua
@@ -3,8 +3,5 @@ require("prototypes.item")
 local storageMultiplier = 4;
 
 data.raw["cargo-wagon"]["cargo-wagon"].inventory_size = math.ceil(data.raw["cargo-wagon"]["cargo-wagon"].inventory_size * storageMultiplier)
-data.raw["cargo-wagon"]["cargo-wagon"].weight         = math.ceil(data.raw["cargo-wagon"]["cargo-wagon"].weight         * storageMultiplier)
-
 data.raw["fluid-wagon"]["fluid-wagon"].capacity       = math.ceil(data.raw["fluid-wagon"]["fluid-wagon"].capacity       * storageMultiplier)
-data.raw["fluid-wagon"]["fluid-wagon"].weight         = math.ceil(data.raw["fluid-wagon"]["fluid-wagon"].weight         * storageMultiplier)
 
